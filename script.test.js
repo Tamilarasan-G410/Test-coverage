dcf68289ca748e050759c90e80480c8153966cfc
Tamilarasan-G410 <fox.4102002@gmail.com>
2024-07-30 14:26:26 +0530

    Refactored test cases

--- script.test.js
+++ script.test.js
@@ -7,134 +7,81 @@ describe('HTML Testing', () => {
     const html = fs.readFileSync(path.resolve(__dirname, 'todolist.html'), 'utf8');
     document.body.innerHTML = html;
   });
-  describe('Title testing', () => {
-    test('Title is displayed', () => {
-      const titleElement = document.querySelector('.title');
-      expect(titleElement).not.toBeNull();
-    });
+  test('All html components are correctly displayed', () => {
+    //query-selectors
+    const titleElement = document.querySelector('.title');
+    const inputElement = document.querySelector('#inputtask');
+    const addButton = document.querySelector('.button');
+    const navBarElement = document.querySelector('nav');
+    const radioButtons = navBarElement.querySelectorAll('input[type="radio"]');
+    const allRadioButton = navBarElement.querySelector('#all');
+    const messageContainer = document.querySelector('.message-container');
+    const noTasksMessage = messageContainer.querySelector('.no-tasks-message');
+    const noAssignedTasksMessage = messageContainer.querySelector('.no-assigned-tasks-message');
+    const noCompletedTasksMessage = messageContainer.querySelector('.no-completed-tasks-message');
+    const showTasks = document.querySelector(".showtasks");
+    const deleteAllButton = document.querySelector('.delete-all');
+    const errormessagecontainer=document.querySelector('.error-message');
+    const toastMessage=document.querySelector('.toast')
+    const overlay=document.querySelector('.overlay')
 
-    test('Content of the title is correct', () => {
-      const titleElement = document.querySelector('.title');
-      expect(titleElement.textContent).toBe('To-Do List');
-    });
-  });
+    //Whether the title is displayed and the check the text content of title
+    expect(titleElement).not.toBeNull();
+    expect(titleElement.textContent).toBe('To-Do List');
 
-  describe('Input box testing', () => {
-    test('Input bar is displayed', () => {
-      const inputElement = document.querySelector('#inputtask');
-      expect(inputElement).not.toBeNull();
-    });
-
-    test('Placeholder of the input box is correct', () => {
-      const inputElement = document.querySelector('#inputtask');
-      expect(inputElement.getAttribute('placeholder')).toBe('Enter the task');
-    });
-
-    test('Auto-complete is off for the input bar', () => {
-      const inputElement = document.querySelector('#inputtask');
-      expect(inputElement.getAttribute('autocomplete')).toBe('off');
-    });
-
-    test('Max length of the input bar is 150', () => {
-      const inputElement = document.querySelector('#inputtask');
-      expect(inputElement.maxLength).toBe(150);
-    });
-  });
-  describe('Add button testing', () => {
-    test('Add button is displayed', () => {
-      const addButton = document.querySelector('.button');
-      expect(addButton).not.toBeNull();
-    });
-
-    test('Title of the add button is displayed', () => {
-      const addButton = document.querySelector('.button');
-      expect(addButton.textContent).toBe('Add');
-    });
-
-    test('Type of the add button is submit', () => {
-      const addButton = document.querySelector('.button');
-      expect(addButton.getAttribute('type')).toBe('submit');
-    });
-  });
-
-  describe('Navigation bar testing', () => {
-    test('Navigation bar is displayed', () => {
-      const navBarElement = document.querySelector('nav');
-      expect(navBarElement).not.toBeNull();
-    });
-
-    test('Three options (all, assigned, completed) are displayed in the navigation bar', () => {
-      const navBarElement = document.querySelector('nav');
-      const radioButtons = navBarElement.querySelectorAll('input[type="radio"]');
-      expect(radioButtons.length).toBe(3);
-    });
-
-    test('Label of the three options are All, Assigned, Completed', () => {
-      const navBarElement = document.querySelector('nav');
-      const radioButtons = navBarElement.querySelectorAll('input[type="radio"]');
-      const labels = [];
-      radioButtons.forEach(radioButton => {
-        const label = document.querySelector(`label[for="${radioButton.id}"]`);
-        if (label) {
-          labels.push(label.textContent);
-        }
-      });
-      expect(labels).toEqual(["All", "Assigned", "Completed"]);
-    });
-
-    test('All option is the default selected one', () => {
-      const navBarElement = document.querySelector('nav');
-      const allRadioButton = navBarElement.querySelector('#all');
-      expect(allRadioButton.checked).toBe(true);
-    });
-  });
-
-  describe('Task board testing', () => {
-    test('No tasks available, No assigned tasks, and No completed messages are initially set to display: none', () => {
-      const messageContainer = document.querySelector('.message-container');
-      const noTasksMessage = messageContainer.querySelector('.no-tasks-message');
-      const noAssignedTasksMessage = messageContainer.querySelector('.no-assigned-tasks-message');
-      const noCompletedTasksMessage = messageContainer.querySelector('.no-completed-tasks-message');
+    //Whether the input text box is displayed and check its attributes
+    expect(inputElement).not.toBeNull();
+    expect(inputElement.getAttribute('placeholder')).toBe('Enter the task');
+    expect(inputElement.getAttribute('autocomplete')).toBe('off');
+    expect(inputElement.maxLength).toBe(150);
     
-      expect(window.getComputedStyle(noTasksMessage).display).toBe('none');
-      expect(window.getComputedStyle(noAssignedTasksMessage).display).toBe('none');
-      expect(window.getComputedStyle(noCompletedTasksMessage).display).toBe('none');
+    //Whether the add button is displayed and check its title and type
+    expect(addButton).not.toBeNull();
+    expect(addButton.textContent).toBe('Add');
+    expect(addButton.getAttribute('type')).toBe('submit');
+    
+    //Whether the nav bar is displayed
+    expect(navBarElement).not.toBeNull();
+    
+    //Whether there are three options in the nav bar and they are all,assigned,completed
+    expect(radioButtons.length).toBe(3);
+    const labels = [];
+    radioButtons.forEach(radioButton => {
+      const label = document.querySelector(`label[for="${radioButton.id}"]`);
+      if (label) {
+        labels.push(label.textContent);
+      }
     });
-
-    test('Task board is displayed', () => {
-      const showTasks = document.querySelector(".showtasks");
-      expect(showTasks).not.toBeNull();
-    });
-  });
-
-  describe('Delete all button testing', () => {
-    test('Delete all button is displayed', () => {
-      const deleteButton = document.querySelector('.delete-all');
-      expect(deleteButton).not.toBeNull();
-    });
-
-    test('Title of the delete all button is displayed', () => {
-      const deleteButton = document.querySelector('.delete-all');
-      expect(deleteButton.textContent).toBe('Delete all Tasks');
-    });
-  });
-  describe('Other components testing',()=>{
-    test('Error message container is present but empty initially',()=>{
-        const errormessagecontainer=document.querySelector('.error-message');
-        expect(errormessagecontainer).not.toBeNull();
-        expect(errormessagecontainer.textContent).toBe('')
-    })
-    test('Toast message container is present',()=>{
-        const toastMessage=document.querySelector('.toast')
-        expect(toastMessage).not.toBeNull();
-    })
-    test('Overlay container is present and not displayed initially',()=>{
-        const overlay=document.querySelector('.overlay')
-        expect(overlay).not.toBeNull();
-        expect(overlay.textContent).toBe('');
-    })
-  })
+    expect(labels).toEqual(["All", "Assigned", "Completed"]);
+    
+    //Whether the all option is selected initially
+    expect(allRadioButton.checked).toBe(true)
+    
+    //whether the display of all no task messages are set to none initially
+    expect(window.getComputedStyle(noTasksMessage).display).toBe('none');
+    expect(window.getComputedStyle(noAssignedTasksMessage).display).toBe('none');
+    expect(window.getComputedStyle(noCompletedTasksMessage).display).toBe('none');
+    
+    //Whether the task board is displayed 
+    expect(showTasks).not.toBeNull();
+    
+    //Whether the delete all button is displayed and check its title
+    expect(deleteAllButton).not.toBeNull();
+    expect(deleteAllButton.textContent).toBe('Delete all Tasks');
+   
+    //Whether the errormessage is present and empty initially
+    expect(errormessagecontainer).not.toBeNull();
+    expect(errormessagecontainer.textContent).toBe('')
+    
+    //Whether the toast message is present and empty initially
+    expect(toastMessage).not.toBeNull();
+    
+    //Whether the overlay is present and empty initially
+    expect(overlay).not.toBeNull();
+    expect(overlay.textContent).toBe('');
+  }); 
 });
+
 describe('Javascript testing', () => {
   let inputBox;
   let showtasks;
@@ -153,7 +100,7 @@ describe('Javascript testing', () => {
     const html = fs.readFileSync(path.resolve(__dirname, 'todolist.html'), 'utf8');
     document.body.innerHTML = html;
   
-
+    
     inputBox = document.querySelector("#inputtask");
     showtasks = document.querySelector(".showtasks");
     form = document.querySelector("#form");
@@ -170,9 +117,11 @@ describe('Javascript testing', () => {
     jest.resetModules();
     require('./script.js');
 
+    // mock timers
     jest.useFakeTimers();
     jest.spyOn(global, 'setTimeout');
 
+    //mock local storage
     const localStorageMock = {
         getItem: jest.fn(),
         setItem: jest.fn(),
@@ -618,66 +567,63 @@ describe('Javascript testing', () => {
   });
 
   describe('showToast Function testing', () => {
-    beforeEach(() => {
-      global.toast = document.querySelector("#toast");
-      global.toastMessage = document.querySelector("#toast-message");
-      global.toastConfirm = document.querySelector("#toast-confirm");
-      global.toastCancel = document.querySelector("#toast-cancel");
-      global.overlay = document.querySelector(".overlay");
-  
-
-      const { showToast, addTask } = require('./script.js'); 
-      global.showToast = showToast;
+    beforeEach(() => { 
+      toast = document.querySelector("#toast");
+      toastMessage = document.querySelector("#toast-message");
+      toastConfirm = document.querySelector("#toast-confirm");
+      toastCancel = document.querySelector("#toast-cancel");
+      overlay = document.querySelector(".overlay");
     });
   
     test('should display toast and overlay, and call onConfirm when confirm button is clicked', () => {
+      const { showToast } = require('./script.js'); 
       const onConfirm = jest.fn();
   
       showToast('Test Message', onConfirm, () => {});
   
-      expect(global.toast.style.display).toBe('flex');
-      expect(global.overlay.style.display).toBe('block');
-      expect(global.toastMessage.textContent).toBe('Test Message');
+      expect(toast.style.display).toBe('flex');
+      expect(overlay.style.display).toBe('block');
+      expect(toastMessage.textContent).toBe('Test Message');
   
       
-      global.toastConfirm.dispatchEvent(new Event('click'));
+      toastConfirm.dispatchEvent(new Event('click'));
   
       expect(onConfirm).toHaveBeenCalled();
-      expect(global.toast.style.display).toBe('none');
-      expect(global.overlay.style.display).toBe('none');
+      expect(toast.style.display).toBe('none');
+      expect(overlay.style.display).toBe('none');
     });
   
     test('should display toast and overlay, and call onCancel when cancel button is clicked', () => {
+      const { showToast } = require('./script.js'); 
       const onCancel = jest.fn();
-  
       showToast('Test Message', () => {}, onCancel);
   
-      expect(global.toast.style.display).toBe('flex');
-      expect(global.overlay.style.display).toBe('block');
-      expect(global.toastMessage.textContent).toBe('Test Message');
+      expect(toast.style.display).toBe('flex');
+      expect(overlay.style.display).toBe('block');
+      expect(toastMessage.textContent).toBe('Test Message');
   
       
-      global.toastCancel.dispatchEvent(new Event('click'));
+      toastCancel.dispatchEvent(new Event('click'));
   
       expect(onCancel).toHaveBeenCalled();
-      expect(global.toast.style.display).toBe('none');
-      expect(global.overlay.style.display).toBe('none');
+      expect(toast.style.display).toBe('none');
+      expect(overlay.style.display).toBe('none');
     });
   
     test('should hide toast and overlay after 10 seconds if no action is taken', () => {
+      const { showToast} = require('./script.js'); 
       jest.useFakeTimers();
-  
       const onCancel = jest.fn();
       showToast('Test Message', () => {}, onCancel);
   
-      expect(global.toast.style.display).toBe('flex');
-      expect(global.overlay.style.display).toBe('block');
+      expect(toast.style.display).toBe('flex');
+      expect(overlay.style.display).toBe('block');
   
      
       jest.advanceTimersByTime(10000);
   
-      expect(global.toast.style.display).toBe('none');
-      expect(global.overlay.style.display).toBe('none');
+      expect(toast.style.display).toBe('none');
+      expect(overlay.style.display).toBe('none');
       expect(onCancel).toHaveBeenCalled();
   
       jest.useRealTimers();
